e4fd3c2a40d44f21795291f5fdc8a4be1574db10
Bruno Baptista <bruno.baptistapt1999@gmail.com>
2021-05-17 15:09:20 +0100

    Added functionality so that only three worded strings are accepted as input

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,26 +3,41 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe("user can only input 3 words", () => {
+  test("when input is empty", () => {
+    render(<App />);
+    const input = screen.getByLabelText("new-sentence") as HTMLInputElement;
+    const button = screen.getByText("Send");
+    fireEvent.change(input, { target: { value: "" } });
+    fireEvent.click(button);
+    expect(screen.queryByText("Hello world! I")).not.toBeInTheDocument();
+  });
+
   test("when user inputs 3 words", () => {
     render(<App />);
     const input = screen.getByLabelText("new-sentence") as HTMLInputElement;
+    const button = screen.getByText("Send");
     fireEvent.change(input, { target: { value: "Hello world! I" } });
+    fireEvent.click(button);
     expect(screen.getByText("Hello world! I")).toBeInTheDocument();
   });
 
   test("when user input 2 words", () => {
     render(<App />);
     const input = screen.getByLabelText("new-sentence") as HTMLInputElement;
+    const button = screen.getByText("Send");
     fireEvent.change(input, { target: { value: "Hello world!" } });
-    expect(screen.getByText("Hello world!")).not.toBeInTheDocument();
+    fireEvent.click(button);
+    expect(screen.queryByText("Hello world!")).not.toBeInTheDocument();
     expect(screen.getByText("Too few words!")).toBeInTheDocument();
   });
 
   test("when user inputs 4 words", () => {
     render(<App />);
     const input = screen.getByLabelText("new-sentence") as HTMLInputElement;
+    const button = screen.getByText("Send");
     fireEvent.change(input, { target: { value: "Hello world! I am" } });
-    expect(screen.getByText("Hello world!")).not.toBeInTheDocument();
+    fireEvent.click(button);
+    expect(screen.queryByText("Hello world!")).not.toBeInTheDocument();
     expect(screen.getByText("Too many words!")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [sentence, setSentence] = useState("");
+  const [text, setText] = useState("");
+  const [error, setError] = useState("");
+
+  function addSentence() {
+    try {
+      checkIfValid(sentence);
+      setText(state => state.concat(sentence + " "))
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      setSentence("");
+    }
+  }
   return (
     <div className="App">
-
+      <div>
+        {text}
+      </div>
+      <label htmlFor="new-sentence" >
+        Insert 3 words
+        <input aria-label="new-sentence" onChange={e => setSentence(e.target.value)} value={sentence} />
+      </label>
+      <button onClick={addSentence} >Send</button>
+      <div>
+        {error}
+      </div>
     </div>
   );
+}
+
+function checkIfValid(word: string): string {
+  const regex = /(?:\w+\W*)/g;
+  const a = word.match(regex);
+  if (!a) throw new Error("Insert three words!")
+  if (a.length < 3) throw new Error("Too few words!");
+  else if (a.length > 3) throw new Error("Too many words!");
+  else return (a[0]);
+
 }
 
 export default App;
